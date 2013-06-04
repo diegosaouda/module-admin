297667db0a49e08072f102f249f72b1c7c97788f
Franck STAUFFER <franck.stauffer@rbs.fr>
2013-06-04 15:07:23 +0200

    Rename Brand vendor to Rbs

---
+++ Setup/Install.php
@@ -0,0 +1,29 @@
+<?php
+namespace Rbs\Brand\Setup;
+
+
+/**
+ * @name \Rbs\Brand\Setup\Install
+ */
+class Install
+{
+	/**
+	 * @param \Change\Plugins\Plugin $plugin
+	 * @param \Change\Application $application
+	 * @throws \RuntimeException
+	 */
+	public function executeApplication($plugin, $application)
+	{
+		/* @var $config \Change\Configuration\EditableConfiguration */
+		$config = $application->getConfiguration();
+		$config->addPersistentEntry('Change/Admin/Listeners/Rbs_Brand', '\\Rbs\\Brand\\Admin\\Register');
+	}
+
+	/**
+	 * @param \Change\Plugins\Plugin $plugin
+	 */
+	public function finalize($plugin)
+	{
+		$plugin->setConfigurationEntry('locked', true);
+	}
+}
